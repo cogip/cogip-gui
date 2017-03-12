1fe7d857f59c66282ac947d3c3f1339c5e90d016
Yannick Gicquel <ygicquel@gmail.com>
2017-03-12 08:28:10 +0100

    mapgraphicsscene: introduc new class
    Signed-off-by: Yannick Gicquel <ygicquel@gmail.com>

---
+++ mapgraphicsscene.cpp
@@ -0,0 +1,6 @@
+#include "mapgraphicsscene.h"
+
+MapGraphicsScene::MapGraphicsScene()
+{
+
+}
---
+++ mapgraphicsscene.h
@@ -0,0 +1,12 @@
+#ifndef MAPGRAPHICSSCENE_H
+#define MAPGRAPHICSSCENE_H
+
+#include <QObject>
+
+class MapGraphicsScene : public QGraphicsScene
+{
+public:
+    MapGraphicsScene();
+};
+
+#endif // MAPGRAPHICSSCENE_H
